4d95735266a796ba8fb7eee2b52a5cf29a4ee02f
Nivaldo <nivaldopvp@gmail.com>
2023-11-20 15:26:16 -0300

    Parte 1: Rotas e Controladores

--- app/Http/Controllers/QuartoController.php
+++ app/Http/Controllers/QuartoController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Quartos;
 
 class QuartoController extends Controller
 {
-    //
+    /**
+     * Método para listar os quartos que estão disponíveis para reserva.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function listarDisponiveis()
+    {
+        try {
+            $quartosDisponiveis = Quartos::where('disponivel', true)->get();
+
+            return response()->json(['quartos_disponiveis' => $quartosDisponiveis], 200);
+        } catch (\Exception $e) {
+            //  Em caso de erro é exibida essa mensagem.
+            return response()->json(['error' => 'Erro ao listar quartos disponíveis.'], 500);
+        }
+    }
 }
