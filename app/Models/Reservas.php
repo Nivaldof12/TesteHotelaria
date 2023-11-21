4fc309c07d08c5a10b18b7406d5512e0041918b9
Nivaldo <nivaldopvp@gmail.com>
2023-11-21 11:10:45 -0300

    Refatoração do código

--- app/Models/Reservas.php
+++ app/Models/Reservas.php
@@ -11,18 +11,6 @@ class Reservas extends Model
 
     protected $fillable = ['data_checkin', 'data_checkout', 'quarto_id', 'cliente_id'];
 
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::saving(function ($reserva) {
-            if ($reserva->exists && $reserva->data_checkin > $reserva->data_checkout) {
-                // Aborta a operação se a data de check-in for maior que a data de check-out
-                return false;
-            }
-        });
-    }
-
     public function quartos()
     {
         return $this->belongsTo(Quartos::class);
